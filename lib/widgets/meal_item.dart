e7826ebd7c83d195587f7069a6a89fb74586b1fd
Krish Choudhary <kdhakla333@gmail.com>
2024-01-22 10:49:14 +0530

    adding meal metadata

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -9,14 +10,24 @@ class MealItem extends StatelessWidget {
   });
   final Meal meal;
 
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);          // If string = "hello" it will return "Hello"
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);          // If string = "hello" it will return "Hello"
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(8),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8),
-      ),      // Even after setting rounded corners we don't get rounded corners
-              // This is because stack by default ignores the shape
+      ), // Even after setting rounded corners we don't get rounded corners
+      // This is because stack by default ignores the shape
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
@@ -58,7 +69,18 @@ class MealItem extends StatelessWidget {
                       height: 10,
                     ),
                     Row(
-                      children: [],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label:
+                              "${meal.duration} min", //meal.duration is in integer so converting it to string
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(width: 12),
+                        MealItemTrait(icon: Icons.attach_money, label: affordabilityText),
+                      ],
                     )
                   ],
                 ),
